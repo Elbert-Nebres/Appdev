7538367e5197aa6ba028d4dba2f93a7f0bcd208e
Enebres <enebres1@gmail.com>
2024-07-22 17:19:10 +0800

    Update login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,11 +7,20 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
+  String _username = '';
   String _password = '';
 
   void _login() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      if (_username == 'name') {
+        // Navigate to the home page (product page) on successful login
+        Navigator.pushReplacementNamed(context, '/home');
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Invalid username')),
+        );
+      }
       if (_password == 'password') {
         // Navigate to the home page (product page) on successful login
         Navigator.pushReplacementNamed(context, '/home');
@@ -27,17 +36,29 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login Page'),
+        title: Text('Login'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextFormField(
-                decoration: InputDecoration(labelText: 'Password'),
+                decoration: InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your username';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _username = value!,
+              ),
+              SizedBox(height: 16.0),
+              TextFormField(
                 obscureText: true,
+                decoration: InputDecoration(labelText: 'Password'),
                 validator: (value) {
                   if (value!.isEmpty) {
                     return 'Please enter your password';
@@ -46,10 +67,15 @@ class _LoginPageState extends State<LoginPage> {
                 },
                 onSaved: (value) => _password = value!,
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 32.0),
               ElevatedButton(
-                child: Text('Login'),
                 onPressed: _login, // Call the _login method
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                  padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 16.0),
+                  textStyle: const TextStyle(fontSize: 18.0),
+                ),
+                child: const Text('Login'),
               ),
             ],
           ),
